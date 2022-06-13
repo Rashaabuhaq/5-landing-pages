e828321a7d1c15dbd9a9b5a239b5043c9b6f5607
RASHA <79843326+Rashaabuhaq@users.noreply.github.com>
2022-06-13 19:54:03 +0300

    five landing pages

---
+++ product/js/js.js
@@ -0,0 +1,14 @@
+var myVideo = document.getElementById("video1");
+
+function playpause() {
+    if (myVideo.paused)
+        myVideo.play();
+    else
+        myVideo.pause();   
+}
+
+$(document).ready(function() {
+    $(function () {
+        $("#commentForm").validate();
+    });
+});
